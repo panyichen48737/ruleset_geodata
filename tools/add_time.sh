1074ba2a89e61274e405ec845cb039fddb016d97
Pandy_Yi <95898667+panyichen48737@users.noreply.github.com>
2024-07-12 15:46:57 +0800

    Create add_time.sh

---
+++ tools/add_time.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 获取当前时间
+current_time=$(date '+%Y-%m-%d %H:%M:%S')
+
+# 目标目录数组
+directories=("../crash" "./another_directory")
+
+# 遍历每个目标目录
+for dir in "${directories[@]}"; do
+  # 遍历目标目录下的所有文件
+  for file in "$dir"/*; do
+    [ -f "$file" ] && sed -i "1s/^/$current_time\n/" "$file"
+  done
+done
+
+echo "时间已添加到所有文件开头。"
