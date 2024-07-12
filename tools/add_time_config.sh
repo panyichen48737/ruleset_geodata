c7f62b0e38eecb0f70fdc7fc09927e545bef20c2
Pandy_Yi <95898667+panyichen48737@users.noreply.github.com>
2024-07-12 16:10:10 +0800

    Create add_time_config.sh

---
+++ tools/add_time_config.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# 获取当前时间（调整为东八区时间）
+current_time=$(date -d '+8 hours' '+%Y-%m-%d %H:%M:%S')
+
+# 目标目录数组
+directories=("../sing-box-config" "../clash-config")
+
+# 遍历每个目标目录
+for dir in "${directories[@]}"; do
+  # 遍历目标目录下的所有文件
+  for file in "$dir"/*; do
+    if [ -f "$file" ]; then
+      # 使用 sed 在文件开头插入带有当前时间的注释
+      sed -i "1s/^/# 生成时间为 $current_time\n/" "$file"
+    fi
+  done
+done
+
+echo "时间已添加到所有文件开头。"
