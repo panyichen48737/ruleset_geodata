2c4c038f16ded14e46531ea8896dfc841f5c6d43
Pandy_Yi <95898667+panyichen48737@users.noreply.github.com>
2024-07-12 15:49:49 +0800

    Update add_time.sh

--- tools/add_time.sh
+++ tools/add_time.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 获取当前时间
-current_time=$(date '+%Y-%m-%d %H:%M:%S')
+current_time=$(date -d '+8 hours' '+%Y-%m-%d %H:%M:%S')
 
 # 目标目录数组
 directories=("../sing-box-ruleset" "../sing-box" "../clash" "../clash-ruleset")
