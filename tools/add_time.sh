21943b6e006d9bbc58227ebaac135314a154584a
Pandy_Yi <95898667+panyichen48737@users.noreply.github.com>
2024-07-13 21:20:55 +0800

    Update add_time.sh

--- tools/add_time.sh
+++ tools/add_time.sh
@@ -8,13 +8,19 @@ directories=("../sing-box-ruleset" "../sing-box" "../clash" "../clash-ruleset")
 
 # 遍历每个目标目录
 for dir in "${directories[@]}"; do
+  echo "正在处理目录: $dir"
   # 遍历目标目录下的所有文件
   for file in "$dir"/*; do
     if [ -f "$file" ]; then
-      # 使用 sed 在文件开头插入带有当前时间的注释
-      sed -i "1s/^/# 生成时间为 $current_time\n/" "$file"
+      case "$file" in
+        *.json|*.list|*.yaml)
+          # 使用 sed 在文件开头插入带有当前时间的注释
+          sed -i "1i # 生成时间为 $current_time" "$file"
+          echo "已处理文件: $file"
+          ;;
+      esac
     fi
   done
 done
 
-echo "时间已添加到所有文件开头。"
+echo "所有符合条件的文件已处理完毕。"
